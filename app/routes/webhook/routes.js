8072d1b5176f46dac366508e0e76dbb05f3ce607
Galaxy97 <atnyehin@gmail.com>
2019-09-18 19:51:44 +0300

    add eslint to poroject

--- app/routes/webhook/routes.js
+++ app/routes/webhook/routes.js
@@ -1,29 +1,33 @@
-const express = require('express')
-const services = require('./services')
-const router = express.Router()
+const express = require('express');
+const services = require('./services');
 
-router.post('/', (req, res) => { // .com/webhook
-  const intent = req.body.queryResult.intent.displayName
-  let fulfillmentText = req.body.queryResult.fulfillmentText
+const router = express.Router();
+
+router.post('/', (req, res) => {
+  // .com/webhook
+  const intent = req.body.queryResult.intent.displayName;
+  let { fulfillmentText } = req.body.queryResult.fulfillmentText;
 
   switch (intent) {
     case 'reminder-save':
-      fulfillmentText = services.saveReminders(req)
-      break
+      fulfillmentText = services.saveReminders(req);
+      break;
     case 'reminder-show':
-      fulfillmentText = services.showReminders()
-      break
+      fulfillmentText = services.showReminders();
+      break;
     case 'reminder-snooze':
-      fulfillmentText = services.snoozeReminders(req)
-      break
+      fulfillmentText = services.snoozeReminders(req);
+      break;
     case 'reminder-delete':
-      fulfillmentText = services.deleteReminders(req)
-      break
+      fulfillmentText = services.deleteReminders(req);
+      break;
     case 'reminder-accept-v2':
-      fulfillmentText = services.acceptReminders()
-      break
+      fulfillmentText = services.acceptReminders();
+      break;
+    default:
+      break;
   }
-  res.json({ fulfillmentText })
-})
+  res.json({ fulfillmentText });
+});
 
-module.exports = router
+module.exports = router;
